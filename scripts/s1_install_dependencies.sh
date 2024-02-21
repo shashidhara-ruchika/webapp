a43db33338f8a15d634515339cdcfdcb80c25253
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-02-21 00:36:41 -0500

    Added echo commands in s1.sh

--- scripts/s1_install_dependencies.sh
+++ scripts/s1_install_dependencies.sh
@@ -7,6 +7,8 @@
 # Install zip
 sudo yum install -y unzip
 
+echo "unzip installation done"
+
 # Install node.js
 sudo yum install -y gcc-c++ make
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
@@ -14,11 +16,15 @@ sudo yum install -y nodejs
 node -v
 npm -v
 
+echo "node.js installation done"
+
 # Install postgres
 sudo yum install -y postgresql-server postgresql-contrib
 sudo postgresql-setup --initdb
 sudo systemctl start postgresql.service
 sudo systemctl enable postgresql.service
+
+echo "postgres installation done"
 
 # Set up db & user
 sudo -u postgres psql -c "CREATE DATABASE app_db;"
@@ -26,3 +32,5 @@ sudo -u postgres psql -c "CREATE USER app_user WITH PASSWORD 'password';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE app_db TO app_user;"
 sudo sed -i.bak 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf
 sudo systemctl restart postgresql
+
+echo "DONE installations"
