7ecaf2ce2184d595647655b8f76822899a848541
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-02-21 02:17:28 -0500

    create env in wf

--- scripts/setup_webapp.sh
+++ scripts/setup_webapp.sh
@@ -4,21 +4,19 @@
 sudo unzip /tmp/webapp.zip -d /opt/csye6225/
 
 # Add env configs
-cd /opt/csye6225/webapp/ || exit 1
-touch .env
-{
-  echo "PORT=8080"
-  echo "DATABASE_NAME=app_db"
-  echo "DATABASE_USER=app_user"
-  echo "DATABASE_PASSWORD=password"
-  echo "DATABASE_HOST=localhost"
-  echo "DATABASE_POOL_MAX=5"
-  echo "DATABASE_POOL_MIN=0"
-  echo "DATABASE_POOL_ACQUIRE=30000"
-  echo "DATABASE_POOL_IDLE=10000"
-  echo "DROP_DATABASE=false"
-} >> .env
-cat .env
+# cd /opt/csye6225/webapp/
+# touch .env
+# echo "PORT=8080" >> .env
+# echo "DATABASE_NAME=app_db" >> .env
+# echo "DATABASE_USER=app_user" >> .env
+# echo "DATABASE_PASSWORD=password" >> .env
+# echo "DATABASE_HOST=localhost" >> .env
+# echo "DATABASE_POOL_MAX=5" >> .env
+# echo "DATABASE_POOL_MIN=0" >> .env
+# echo "DATABASE_POOL_ACQUIRE=30000" >> .env
+# echo "DATABASE_POOL_IDLE=10000" >> .env
+# echo "DROP_DATABASE=false" >> .env
+# cat .env
 
 # Install node packages
 sudo npm install
