f3372bd61ecc347fd9309cf7495dc5d70f0b8d62
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-02-21 01:10:42 -0500

    refactor sh

--- scripts/create_csye6225_user.sh
+++ scripts/create_csye6225_user.sh
@@ -3,11 +3,13 @@
 # Create User & Group
 sudo groupadd -f csye6225
 # sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/csye6225 -m csye6225
-sudo adduser csye6225 --shell /usr/sbin/nologin -g csye6225 -d /opt/csye6225 -m csye6225
+# sudo adduser csye6225 --shell /usr/sbin/nologin -g csye6225 -d /opt/csye6225 -m csye6225
+sudo useradd -s /usr/sbin/nologin -g csye6225 -d /opt/csye6225 -m csye6225
+
 
 # sudo mkdir /opt/csye6225
 
 # Provide Access
-sudo chown -R csye6225:csye6225 /opt/csye6225
-sudo chmod -R 775 /opt/csye6225
-sudo chmod g+s /opt/csye6225
+sudo chown -R csye6225:csye6225 /opt/csye6225/
+sudo chmod -R 775 /opt/csye6225/
+# sudo chmod g+s /opt/csye6225/
