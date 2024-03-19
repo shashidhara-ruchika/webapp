3fd68aea763b4455bd4a8657a67b131fa278c677
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-03-19 01:20:09 -0400

    create log dir script fix

--- scripts/setup_log_dirs.sh
+++ scripts/setup_log_dirs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Create Logs Directory
-sudo mkdir /var/log/csy6225
-sudo mkdir /var/log/csy6225/webapp
+sudo mkdir /var/log/csye6225
+sudo mkdir /var/log/csye6225/webapp
 
 # Provide Logs Directory Access
-sudo chown -R csye6225:csye6225 /var/log/csy6225/
-sudo chmod -R 775 /var/log/csy6225/
+sudo chown -R csye6225:csye6225 /var/log/csye6225/
+sudo chmod -R 775 /var/log/csye6225/
