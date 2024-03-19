24fb29a87e1300ede35f2aa0ce2ff9012f1b977b
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-03-19 14:40:57 -0400

    cp config.yaml

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -25,7 +25,8 @@ curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 # Set up ops agent for webapp logs
-# sudo cat /tmp/config.yaml >> /etc/google-cloud-ops-agent/config.yaml
-su -c "cat /tmp/config.yaml >> /etc/google-cloud-ops-agent/config.yaml"
-su -c "cat /etc/google-cloud-ops-agent/config.yaml"
+sudo cat /tmp/config.yaml >> /etc/google-cloud-ops-agent/config.yaml
+# su -c "cat /tmp/config.yaml >> /etc/google-cloud-ops-agent/config.yaml"
+echo "cat /etc/google-cloud-ops-agent/config.yaml"
+sudo "cat /etc/google-cloud-ops-agent/config.yaml"
 sudo systemctl restart google-cloud-ops-agent
