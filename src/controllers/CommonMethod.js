2fe141fe69e152db0c56feb1d7434ec06c4665dd
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2024-03-20 18:31:09 -0400

    update logger level

--- src/controllers/CommonMethod.js
+++ src/controllers/CommonMethod.js
@@ -1,7 +1,7 @@
 import { logger } from "../services/LoggerService.js";
 
 export const handleMethodNotAllowed = async (req, res) => {
-  logger.error(`Method Not Allowed: ${req.method}`);
+  logger.warn(`[Method Not Allowed] ${req.method}`);
   res.status(405).end();
   return res;
 };
